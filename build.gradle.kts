4e0ceec7caa5f9a06bca8aca23cfb7c382a51441
tjdgus903 <158554431+tjdgus903@users.noreply.github.com>
2026-09-11 12:09:30 +0900

    Expand test exception logging for Java PSI CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.intellij.platform.gradle.TestFrameworkType
 
 plugins {
@@ -40,4 +42,11 @@ java {
 
 tasks.test {
     useJUnitPlatform()
+    testLogging {
+        events = setOf(TestLogEvent.FAILED)
+        exceptionFormat = TestExceptionFormat.FULL
+        showExceptions = true
+        showCauses = true
+        showStackTraces = true
+    }
 }
