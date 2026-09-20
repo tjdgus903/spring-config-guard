71e115fd8a5929a8d7cd61690df4c01379aa7256
tjdgus903 <158554431+tjdgus903@users.noreply.github.com>
2026-09-20 18:08:43 +0900

    build: upgrade IntelliJ Platform Gradle Plugin to 2.19.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.jetbrains.intellij.platform.gradle.TestFrameworkType
 
 plugins {
     java
-    id("org.jetbrains.intellij.platform") version "2.18.1"
+    id("org.jetbrains.intellij.platform") version "2.19.0"
     kotlin("jvm") version "2.4.20"
 }
 
